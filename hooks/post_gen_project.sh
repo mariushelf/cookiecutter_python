71c00188d7021cf7685eefc3a2c22004f7b252fd
Marius Helf <marius@xomnia.com>
2020-11-05 18:55:22 +0100

    install dependencies and pre-commit hooks after initialization
    fix #2

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+echo Initializing and configuring git...
 git init
 git config --local user.name "{{cookiecutter.full_name}}"
 git config --local user.email {{cookiecutter.email}}
 git remote add origin git@github.com:{{cookiecutter.github_username}}/{{cookiecutter.project_slug}}.git
+echo Installing package dependencies locally...
+poetry install
+echo Installing pre-commit hooks...
+poetry run pre-commit install
+echo Done. Happy coding!
