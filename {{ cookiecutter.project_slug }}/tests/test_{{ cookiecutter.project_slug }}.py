c5c2c28c20cbd738ee1f02eb0c49b687a8a55d3f
Marius Helf <marius.helf@enexis.nl>
2022-04-07 11:18:17 +0200

    updates to build and test framework

---
+++ {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.project_slug }}.py
@@ -0,0 +1,6 @@
+# The test pipeline will fail if there are no tests, so here we have
+# a dummy file with a single test that always fails.
+# TODO: implement proper tests here!
+
+def test_reminder():
+    assert False, "If this test runs it probably means that you have not written your own tests yet. Do it now!"
