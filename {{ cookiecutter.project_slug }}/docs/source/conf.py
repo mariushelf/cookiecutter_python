070d5362a986beb30368c1804df0c9a221157958
Marius Helf <marius.helf@enexis.nl>
2022-04-15 18:34:21 +0200

    read version from pyproject.toml

--- {{ cookiecutter.project_slug }}/docs/source/conf.py
+++ {{ cookiecutter.project_slug }}/docs/source/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
+import {{ cookiecutter.project_slug }}  # noqa: E402
 
 
 # -- Project information -----------------------------------------------------
@@ -23,7 +24,8 @@ copyright = "{% now 'utc', '%Y' %}, {{ cookiecutter.full_name }}"
 author = "{{ cookiecutter.full_name }}"
 
 # The full version, including alpha/beta/rc tags
-release = "{{ cookiecutter.version }}"
+release = {{ cookiecutter.project_slug }}.__version__
+version = {{ cookiecutter.project_slug }}.__version__
 
 
 # -- General configuration ---------------------------------------------------
--- {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.project_slug }}.py
+++ {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.project_slug }}.py
@@ -1,6 +1,12 @@
-# The test pipeline will fail if there are no tests, so here we have
-# a dummy file with a single test that always fails.
-# TODO: implement proper tests here!
+import re
+
+# TODO: implement your tests here!
 
 def test_reminder():
     assert False, "If this test runs it probably means that you have not written your own tests yet. Do it now!"
+
+def test_version_is_semver_string():
+    semver_pattern = r'^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
+    version = {{ cookiecutter.project_slug }}.__version__
+    print(f"Version is {version}")
+    assert re.match(semver_pattern, version)
