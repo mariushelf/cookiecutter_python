e80027723600ef2abaf24d43b639336190e5324b
Marius Helf <marius@happyyeti.tech>
2023-02-27 11:19:46 +0100

    add docstrings to test functions

--- {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.project_slug }}.py
+++ {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.project_slug }}.py
@@ -6,12 +6,14 @@ import {{ cookiecutter.project_slug }}
 
 
 def test_reminder():
+    """Reminder to write tests."""
     assert (
         False
     ), "If this test runs it probably means that you have not written your own tests yet 😱 Do it now!"
 
 
 def test_version_is_semver_string():
+    """Test that the version in pyproject.toml is a proper semantic version."""
     semver_pattern = r"^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
     version = {{ cookiecutter.project_slug }}.__version__
     print(f"Version is {version}")
